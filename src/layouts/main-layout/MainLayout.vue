3da6f57d0d3b3085f9c7fa9c13dc7d7569b4f975
Ricardo Mancilla Morales <ricardo.mancilla@btob.com.mx>
2023-09-04 21:47:11 -0600

    project basis

---
+++ src/layouts/main-layout/MainLayout.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="main-view">
+        <h1>Main Layout</h1>
+    </div>
+</template>
+
+<script lang="ts">
+    import { defineComponent } from "vue";
+    
+    export default defineComponent({
+        name: "main-layout",
+    });
+</script>
